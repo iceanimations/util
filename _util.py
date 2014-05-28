001af715748ddce24a1ee4504b4f5920ef49bf36
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-05-28 09:35:37 +0500

    plenty of util function shot management added. Minor bug in all_tasks/get_tasks fixed.
    Functions:
    - get_assets
    - get_assets_in_shot
    - get_shots
    - get_tasks
    - get_sequences
    - get_episodes

--- _util.py
+++ _util.py
@@ -1,3 +1,7 @@
+'''
+This module can work both on server and client side.
+'''
+
 from site import addsitedir as asd
 asd(r"r:/Pipe_Repo/Users/Hussain/utilities/TACTIC")
 from datetime import datetime
@@ -11,36 +15,46 @@ try:
     from auth import user as USER
     user = USER.get_user()
     set_server(USER.get_server())
+    
     def get_all_task(user = USER.get_user()):
+        
+        user = USER.get_user()
         return [task.get("__search_key__", '') for proj in get_all_projects()
                 for task in all_tasks(proj, user = user)]
-        
+
     def get_all_project_user_sobjects(user = USER.get_user()):
+        '''
+        
+        '''
+        user = USER.get_user()
         proj_dict = {}
         for proj in get_all_projects():
             key = proj['code']
             sobj = get_user_related_sobjects(proj, user)
             if sobj:
                 proj_dict[key] = sobj
+                
         return proj_dict
 
     def get_user_related_sobjects(proj, user = USER.get_user()):
         '''
-        @return: sobject_search_key
+        @return: sobject_search_key list
         '''
-        return map_tasks_to_sobjects([task
-                                      for task in all_tasks(proj, user = user)
-                                      if str(task["status"]).lower() != "done"]).keys()
+        user = USER.get_user()
+        return map_tasks_to_sobjects(
+            [task
+             for task in all_tasks(proj, user = user)
+             if str(task["status"]).lower() != "done"]).keys()
 
     def sobject_to_user_task(sobj, user = USER.get_user()):
-
+        user = USER.get_user()
         proj = _s.get_project()
         start = sobj.find('project=')
         project = sobj[start:start + sobj[start:].find('&')]
-        _s.set_project(project.replace("project=", ""))
+        set_project(project = project.replace("project=", ""))
         tasks = _s.get_all_children(sobj, "sthpw/task",
                                     filters = [("assigned", str(user))])
-        _s.set_project(proj)
+        set_project(project = proj)
         return [task.get('__search_key__') for task in tasks]
 
 
@@ -49,17 +63,44 @@ except:
     from client.tactic_client_lib import TacticServerStub
     set_server(TacticServerStub.get())
 
-def set_project(project = None, search_key = None):
-    
-    print project, search_key
-    
-    server = _s
-    
-    if project and project != server.get_project():
-        server.set_project(project)
-    elif search_key:
-        server.set_project(get_project_from_search_key(search_key))
-    
+def all_task_processes(project, tasks = None):
+    processes = set([task["process"] for task in (all_tasks(project)
+                                                  if not tasks else tasks)])
+    return list(processes)
+
+def all_process_tasks(project, process = None, tasks = None):
+    processes = set([task["process"] for task in (all_tasks(project)
+                                                  if not tasks else tasks)])
+    tasks = {}
+    for proc in list(processes):
+        # get rid of this call to all_tasks
+        t = all_tasks(project, proc)
+        tasks[proc] = t
+
+    return tasks[process] if process else tasks
+
+def current_project():
+    return _s.get_project()
+
+def date_str_to_datetime(string, format = "%Y-%m-%d %H:%M:%S"):
+    return datetime.strptime(string.split(".")[0], format)
+
+def filename_from_snap(snap, mode = 'sandbox'):
+    '''
+    @snap: db dict
+    '''
+    set_project(project = snap['project_code'])
+    return _s.get_all_paths_from_snapshot(snap['__search_key__'],
+                                          mode = mode)[0]
+
+# def filter_user_tasks(user, tasks):
+#     '''
+#     Extract the tasks that belong ot `user' from a list `tasks'.
+#     @user: the user who's tasks are to be extracted
+#     @tasks: list of task
+#     '''
+#     return filter(lambda task: task["assigned"] == user, tasks)
+
 def get_project_from_search_key(s_key):
     
     if 'project' in s_key:
@@ -69,32 +110,6 @@ def get_project_from_search_key(s_key):
     else:
         return 'sthpw'
     
-def get_server(): return _s
-def get_all_projects():
-    project = _s.get_project()
-    _s.set_project("sthpw")
-    projects = _s.query("sthpw/project")
-    _s.set_project(project)
-    map(projects.pop, [ind for ind in reversed(range(len(projects)))
-                       if (# filter 'admin' and 'sthpw'
-                               projects[ind]["code"] in ["admin", "sthpw"]
-                               # filter all template project
-                               or projects[ind]["is_template"]
-                               # filter all Sample Projects
-                               or projects[ind]['category'] == 'Sample Projects')
-                       # make exception for the vfx project
-                       and not projects[ind]['code'] == 'vfx'])
-    return projects
-
-def map_sobject_to_snap(sobjs):
-    sobj_map = {}
-    for sobj in sobjs:
-        try:
-            sobj_map[sobj] = get_snapshot_from_sobject(sobj)
-        except:
-            sobj_map[sobj] = []
-    return sobj_map
-
 # def get_all_projects(clean = False):
 #     projects = all_projects()
 #     if clean:
@@ -107,14 +122,27 @@ def map_sobject_to_snap(sobjs):
 #         for proj in projects:
 #             proj["start_date"] = all
 #     return projects
+def get_server():
+    return _s
 
-def project_start_date(project):
-    pass
-
-def team_involved(project):
-    pass
+def get_all_projects():
+    project = _s.get_project()
+    set_project(project = "sthpw")
+    projects = _s.query("sthpw/project")
+    set_project(project = project)
+    map(projects.pop, [ind for ind in reversed(range(len(projects)))
+                       if (# filter 'admin' and 'sthpw'
+                               projects[ind]["code"] in ["admin", "sthpw"]
+                               # filter all template project
+                               or projects[ind]["is_template"]
+                               # filter all Sample Projects
+                               or projects[ind]['category'] == 'Sample Projects')
+                       # make exception for the vfx project
+                       and not projects[ind]['code'] == 'vfx'])
+    return projects
 
 def get_sobject_from_task(task):
+    
     '''
     @task: task dict or task string
     '''
@@ -129,43 +157,8 @@ def get_sobject_from_task(task):
                                    task["search_code"],
                                    project_code = task.get("project_code"))
 
-def map_tasks_to_sobjects(tasks):
-
-    '''
-    @return: {sobject_search_key: [tasks_database_dicts,...]}
-    '''
-    s_tasks = {}
-    for task in tasks:
-        
-        sobj = get_sobject_from_task(task)
-        if not sobj: continue
-        try:
-            s_tasks[sobj].append(task)
-        except:
-            s_tasks[sobj] = [task]
-    return s_tasks
-
-# def filter_user_tasks(user, tasks):
-#     '''
-#     Extract the tasks that belong ot `user' from a list `tasks'.
-#     @user: the user who's tasks are to be extracted
-#     @tasks: list of task
-#     '''
-#     return filter(lambda task: task["assigned"] == user, tasks)
-
-def project_code_to_title(proj_code):
-    '''
-    @return: the project title given it's code. None if the project code is not listed.
-    '''
-    return _s.query("sthpw/project", [("code", proj_code)], single = True).get("title")
-
-def project_from_tasks(tasks):
-    '''
-    @tasks: task database column dict
-    '''
-    return set([task["project_code"] for task in tasks])
-
 def get_project_from_task(task):
+    
     return _s.get_by_search_key(task)['project_code']
 
 def get_snapshot_from_sobject(sobj):
@@ -199,12 +192,14 @@ def get_sobject_from_snap(snap):
                                project_code = snap.get("project_code"))
 
 def get_sobject_name(sobj):
+    
     sobj_dict =  _s.get_by_search_key(sobj)
     title =  sobj_dict.get("title",
                            sobj_dict.get("name",
                                          'No title'))
     # because of the changes in the new release
     return title + '(%s)'%sobj_dict.get('code')
+
 # def add_snapshot_to_task(sobjs_to_tasks):
 
 #     '''
@@ -233,7 +228,7 @@ def get_sobject_name(sobj):
                 
 #         snapshoted[sobj] = task_snap
 #     return snapshoted
-def all_tasks(project, process = None, user = None, clean = False):
+def get_tasks(project, process = None, user = None, clean = False):
 
     '''
     @project: takes in the project code
@@ -241,8 +236,7 @@ def all_tasks(project, process = None, user = None, clean = False):
     '''
     
     proj = current_project()
-    _s.set_project(project["code"])
-    
+    set_project(project = project["code"])
     filters = [("process", process)] if process else []
     filters.append(("project_code", project["code"]))
 
@@ -250,53 +244,93 @@ def all_tasks(project, process = None, user = None, clean = False):
         filters.append(("assigned", _s.get_login()))
     elif user:
         filters.append(("assigned", user))
-    else: pass
+    else:
+        pass
     
-    tasks = _s.query("sthpw/task", filters = filters)
-    if clean:
-        pop = [drat for drat in range(len(tasks))
-               if "?project=%s" %project not in str(tasks[drat]["search_type"])]
-        map(tasks.pop, reversed(pop))
-    _s.set_project(proj)
+    tasks = []
+    print "project:", _s.get_project()
+    try:
+        tasks = _s.query("sthpw/task", filters = filters)
+        if clean:
+            pop = [drat for drat in range(len(tasks))
+                   if "?project=%s" %project not in
+                   str(tasks[drat]["search_type"])]
+            map(tasks.pop, reversed(pop))
+    except:
+        print 'ERROR!!!!'
+        pass
+    
+    pretty_print(tasks)
+    set_project(project = proj)
+    
     return tasks
 
-def all_assets(project):
+def get_episodes(project):
+    '''
+    @project: project search key
+    @return: list of all episode db dicts
+    '''
     proj = current_project()
-    _s.set_project(project)
-    result =  _s.query('vfx/asset'# , filters = [("project_code", project)]
-    )
-    _s.set_project(proj)
+    set_project(project = project)
+    result = _s.query('vfx/episode')
+    set_project(project=proj)
     return result
 
-def get_project_snapshots(proj):
-    return _s.query('sthpw/snapshot', filters = [('project_code', proj)])
+def get_sequences(project, episode = None):
+    '''
+    @project: project search key
+    @episode: episode search key
+    @return: list of filtered sequence db dicts
+    '''
+    proj = current_project()
+    set_project(project = project)
+    result = _s.query('vfx/sequence',
+                      filters = [('episode_code', episode)]
+                      if episode else [])
+    set_project(project = proj)
+    return result
 
-def task_details(project):
-    pass
+def get_shots(project, sequence = None):
+    '''
+    @project: project search key
+    @sequence: sequence search key
+    @return: list of filtered shot db dicts
+    '''
+    proj = current_project()
+    set_project(project = project)
+    result = _s.query('vfx/shot',
+                      filters = [('sequence_code', sequence)]
+                      if sequence else [])
+    set_project(project = proj)
+    return result
 
-def production_dept(project):
-    pass
+def get_assets(project):
+    proj = current_project()
+    set_project(project = project)
+    result =  _s.query('vfx/asset'# , filters = [("project_code", project)]
+    )
+    set_project(project = proj)
+    return result
 
-def current_project(): return _s.get_project()
-def all_task_processes(project, tasks = None):
-    processes = set([task["process"] for task in (all_tasks(project)
-                                                  if not tasks else tasks)])
-    return list(processes)
-
-def all_process_tasks(project, process = None, tasks = None):
-    processes = set([task["process"] for task in (all_tasks(project)
-                                                  if not tasks else tasks)])
-    tasks = {}
-    for proc in list(processes):
-        # get rid of this call to all_tasks
-        t = all_tasks(project, proc)
-        tasks[proc] = t
-
-    return tasks[process] if process else tasks
-
-def date_str_to_datetime(string, format = "%Y-%m-%d %H:%M:%S"):
-    return datetime.strptime(string.split(".")[0], format)
-
+def get_assets_in_shot(project, shot):
+    '''
+    @project: project search key
+    @shot: shot search key
+    @return: list of asset db dicts
+    '''
+    proj = current_project()
+    set_project(project = project)
+    result = map(_s.get_by_search_key,
+                 map(lambda code: _s.build_search_key('vfx/asset',
+                                                      code['asset_code'],
+                                                      project_code = project),
+                     _s.query('vfx/asset_in_shot',
+                              filters = [('shot_code',
+                                          _s.get_by_search_key(shot)['code'])],
+                             columns = ['asset_code'])))
+    set_project(proj)
+    return result
+    
 def get_project_title(proj_code):
     result = _s.query("sthpw/project", filters = [("code", proj_code)])
     if result:
@@ -349,17 +383,73 @@ def get_snapshots(context, task):
         }
     return snapshot_dict
 
-def filename_from_snap(snap, mode = 'sandbox'):
-    '''
-    @snap: db dict
-    '''
-    set_project(project = snap['project_code'])
-    return _s.get_all_paths_from_snapshot(snap['__search_key__'],
-                                          mode = mode)[0]
-                                          
+def get_project_snapshots(proj):
+    return _s.query('sthpw/snapshot', filters = [('project_code', proj)])
+
 def get_all_users():
     return _s.query("sthpw/login")
     
+def map_sobject_to_snap(sobjs):
+    sobj_map = {}
+    for sobj in sobjs:
+        try:
+            sobj_map[sobj] = get_snapshot_from_sobject(sobj)
+        except:
+            sobj_map[sobj] = []
+    return sobj_map
+
+def map_tasks_to_sobjects(tasks):
+
+    '''
+    @return: {sobject_search_key: [tasks_database_dicts,...]}
+    '''
+    s_tasks = {}
+    for task in tasks:
+        
+        sobj = get_sobject_from_task(task)
+        if not sobj: continue
+        try:
+            s_tasks[sobj].append(task)
+        except:
+            s_tasks[sobj] = [task]
+    return s_tasks
+
+def production_dept(project):
+    pass
+
+def project_start_date(project):
+    pass
+
+def project_code_to_title(proj_code):
+    '''
+    @return: the project title given it's code. None if the project code is not listed.
+    '''
+    return _s.query("sthpw/project", [("code", proj_code)], single = True).get("title")
+
+def project_from_tasks(tasks):
+    '''
+    @tasks: task database column dict
+    '''
+    return set([task["project_code"] for task in tasks])
+
 def pretty_print(obj, ind = 2):
     import json
     print json.dumps(obj, indent = ind)
+
+def set_project(project = None, search_key = None):
+
+    server = _s
+    
+    if project and project != server.get_project():
+        server.set_project(project)
+    elif search_key:
+        server.set_project(get_project_from_search_key(search_key))
+
+def task_details(project):
+    pass
+
+def team_involved(project):
+    pass
+
+all_assets = get_assets
+all_tasks = get_tasks
